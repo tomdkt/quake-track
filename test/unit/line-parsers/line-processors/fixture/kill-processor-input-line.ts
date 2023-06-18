57779c360cacdac84c6527a2c21cf3278080647e
Thomas Teixeira <thomas.teixeira@loft.com.br>
2023-06-18 16:35:27 -0300

    test: when the world kills

--- test/unit/line-parsers/line-processors/fixture/kill-processor-input-line.ts
+++ test/unit/line-parsers/line-processors/fixture/kill-processor-input-line.ts
@@ -1,2 +1,5 @@
 export const inputSelfDestruction =
   '22:18 Kill: 2 2 7: Isgalamido killed Isgalamido by MOD_ROCKET_SPLASH';
+
+export const inputWorldKills =
+  '23:06 Kill: 1022 2 22: <world> killed Isgalamido by MOD_TRIGGER_HURT';
--- test/unit/line-parsers/line-processors/kill-processor.spec.ts
+++ test/unit/line-parsers/line-processors/kill-processor.spec.ts
@@ -4,7 +4,7 @@ import { GameRepository } from '../../../../src/repository/game-repository';
 import { instance, mock, reset, verify, when } from 'ts-mockito';
 import { newPlayerStats } from './fixture/new-player-stats-fixture';
 import { KillProcessor } from '../../../../src/line-parsers/line-processors/kill-processor';
-import { inputSelfDestruction } from './fixture/kill-processor-input-line';
+import { inputSelfDestruction, inputWorldKills } from './fixture/kill-processor-input-line';
 
 describe('KillProcessor', () => {
   let component: KillProcessor;
@@ -52,6 +52,40 @@ describe('KillProcessor', () => {
           },
           kills_by_means: {
             MOD_ROCKET_SPLASH: 1,
+          },
+        });
+      });
+    });
+
+    describe('when the world kills', () => {
+      const input = inputWorldKills;
+      const playerStats = newPlayerStats({
+        playerNames: { '2': 'Isgalamido' },
+        kills: { Isgalamido: 0 },
+      });
+
+      before(async () => {
+        reset(gameRepository);
+        when(gameRepository.getPlayerStats()).thenReturn(playerStats);
+        component.processLine(input);
+      });
+
+      it('should call getPlayerStats', async () => {
+        verify(gameRepository.getPlayerStats()).called();
+      });
+
+      it('should change the playerStats', async () => {
+        expect(playerStats).to.be.eql({
+          total_kills: 1,
+          players: [],
+          playerNames: {
+            '2': 'Isgalamido',
+          },
+          kills: {
+            Isgalamido: -1,
+          },
+          kills_by_means: {
+            MOD_TRIGGER_HURT: 1,
           },
         });
       });
